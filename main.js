b1d9307469157d3f031d7442db90c6af21676062
Jerry Jones IV <jjonesiv21@gmail.com>
2022-11-03 17:43:40 -0400

    static scatter plot is up

--- main.js
+++ main.js
@@ -1,13 +1,34 @@
 //var main = d3.select('#main');
+irisLength = []
+irisWidth = []
+
 
 d3.csv('iris.csv').then(function(dataset) {
-    console.table(dataset)
-    console.log("Line 1: inside the callback function");
+    //console.table(dataset)
+
+    for (i=0;i<dataset.length;i++){
+        irisLength.push(dataset[i]['SepalLengthCm'])
+        irisWidth.push(dataset[i]['SepalWidthCm'])
+    }
 
     var g = svg.selectAll("g")
     .data(dataset)
     .enter()
     .append("g")
+    .attr("transform", function(d) {
+    return ("translate(" + scaleLength(d.SepalLengthCm) + "," + scaleWidth(d.SepalWidthCm) + ")")
+    })
+
+    /*
+    var text = svg.selectAll("g")
+    .data(dataset)
+    .enter()
+    g.append("text")
+    .attr('class', 'info')
+    .text(function(d) {
+    return (d.Id)
+    })
+     */
 
     var circles = svg.selectAll("g")
     .data(dataset)
@@ -15,19 +36,38 @@ d3.csv('iris.csv').then(function(dataset) {
     g.append("circle")
     .attr("r", 2)
 
-})
+});
 
-/*
+function scaleLength(SepalLengthCm) {
+    return lengthScale(SepalLengthCm);
+}
 
-var trace1 = {
-  x: [1, 2, 3, 4],
-  y: [10, 15, 13, 17],
-  mode: 'markers',
-  type: 'scatter'
-};
+function scaleWidth(SepalWidthCm) {
+    return widthScale(SepalWidthCm);
+}
+var svg = d3.select('svg');
 
-var data = [trace1];
+var lengthScale = d3.scaleLinear()
+    .domain([3.3,8.9]).range([60,700]);
 
-Plotly.newPlot('chart', data);
+var widthScale = d3.scaleLinear()
+    .domain([1,5.4]).range([340,20]);
 
- */
+svg.append('g').attr('class', 'x axis')
+    .attr('transform', 'translate(0,345)')
+    .call(d3.axisBottom(lengthScale).tickFormat(function(d){return d;}));
+
+svg.append('g').attr('class', 'y axis')
+    .attr('transform', 'translate(55,0)')
+    .call(d3.axisLeft(widthScale));
+
+svg.append('text')
+    .attr('class', 'label')
+    .attr('transform','translate(360,390)')
+    .text('Sepal Length');
+
+svg.append('text')
+    .attr('class', 'label')
+    .attr('transform','translate(15,200) rotate(90)')
+    .text('Sepal Width');
+
