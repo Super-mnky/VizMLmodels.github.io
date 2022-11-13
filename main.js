f2c8666044ee615118af4976fb22a7921bf4b96f
kaitlyn019 <kaitlynyang16@gmail.com>
2022-11-13 16:58:35 -0500

    data prep: petal and sepal lengths

--- main.js
+++ main.js
@@ -3,7 +3,11 @@ var current_viz = 0
 var viz_ids = [
   '#viz_1_1',
   '#viz_1_2',
-  '#viz_1_3'
+  '#viz_1_3', '#viz_1_3'
+]
+
+var viz_fns = [
+  viz11, viz12, viz13, viz14
 ]
 
 d3.graphScroll()
@@ -19,6 +23,13 @@ function updateViz(i) {
   d3.select(viz_ids[current_viz]).style('display', 'none')
   d3.select(viz_ids[i]).style('display','block')
   current_viz = i
+  viz_fns[i]();
+}
+
+function delay(milliseconds){
+  return new Promise(resolve => {
+      setTimeout(resolve, milliseconds);
+  });
 }
 
 // Getting the data via promise so you can work through irises as well to get the data
@@ -130,8 +141,6 @@ function viz11(){
   });
 }
 
-viz11();
-
 function viz12(){
 
   var lengthScale = d3.scaleLinear()
@@ -200,7 +209,7 @@ function viz12(){
       svg.selectAll("g")
           .data(dataset)
           .enter()
-          svg.selectAll("circle")
+      svg.selectAll("circle")
           .transition()
           .duration(750)
           .attr("transform", function(d) {
@@ -220,46 +229,152 @@ function viz12(){
   });
 }
 
-viz12();
-
 function viz13(){
   var svg = d3.select("#viz_1_3")
   .append('svg')
   .attr('width', width)
-  .attr('height', height)    
-var img = svg.append('image')
+  .attr('height', height)  
+
+  var defs = svg.append('defs')
+  
+  defs.append('marker').attr('id','startarrow').attr('orient','auto').attr('markerHeight', '4')
+      .attr('refY', '2').attr('refX', '0').attr('markerWidth','4').attr('markerUnits','strokeWidth')
+      .append('polygon').attr('fill','white').attr('points', '4 0, 4 4, 0 2')
+
+  defs.append('marker').attr('id','endarrow').attr('orient','auto').attr('markerHeight', '4')
+      .attr('refY', '2').attr('refX', '0').attr('markerWidth','4').attr('markerUnits','strokeWidth')
+      .append('polygon').attr('fill','white').attr('points', '0 0, 4 2, 0 4')
+
+  var img = svg.append('image')
    .attr('href', 'static/versicolor-photo.jpg')
    .attr('height', '50%')
    .attr('width', '50%')
    .attr('transform', 'translate(170,120)')
 
-const transitionPath = d3.transition().ease(d3.easeSin).duration(2500);
-var sepallaxis = svg.append('line')
-    .attr('stroke', 'white').attr('stroke-width', '3')
-    .attr('stroke-dashoffset', '30')
-    .attr('x1', '325').attr('x2', '320')
-    .attr('y1','360').attr('y2', '260')
+  const transitionPath = d3.transition().ease(d3.easeSin).duration(1000);
+  var sepallaxis = svg.append('line').attr('class','sepal')
+    .attr('stroke', 'white').attr('stroke-width', '2')
+    //.attr('stroke-dashoffset', '5').attr('stroke-dasharray', '4')
+    .attr('x1', '325').attr('x2', '325')
+    .attr('y1','260').attr('y2', '260')
+    .attr('marker-start','url(#startarrow)').attr('marker-end','url(#endarrow)')
     .transition(transitionPath)
-    .attr('stroke-dashoffset', '0')
+    .attr('y1', '360')
 
-var sepalwaxis = svg.append('line')
-    .attr('stroke', 'white').attr('stroke-width', '3')
-    .attr('x1', '355').attr('x2', '280')
+  var sepalwaxis = svg.append('line').attr('class','sepal')
+    .attr('stroke', 'white').attr('stroke-width', '2')
+    .attr('x1', '285').attr('x2', '285')
     .attr('y1','290').attr('y2', '290')
+    .attr('marker-start','url(#startarrow)').attr('marker-end','url(#endarrow)')
+    .transition(transitionPath)
+    .attr('x1', '355')
 
+  svg.append('text').attr('class','label').attr('fill','white')
+  .attr('transform', 'translate(300,375)').text('Sepal').attr('opacity','0')
+  .transition(transitionPath)
+  .attr('opacity','1')
 
-svg.append('text').attr('class','label').attr('fill','white')
-.attr('transform', 'translate(300,375)').text('Sepal')
+  var petallaxis = svg.append('line').attr('class','petal')
+  .attr('stroke', 'white').attr('stroke-width', '2')
+  .attr('x1', '335').attr('x2', '335')
+  .attr('y1','155').attr('y2', '155')
+  .attr('marker-start','url(#startarrow)').attr('marker-end','url(#endarrow)')
+  .transition(transitionPath)
+  .attr('y2', '240')
 
+  var petalwaxis = svg.append('line').attr('class','petal')
+    .attr('stroke', 'white').attr('stroke-width', '2')
+    .attr('x1', '320').attr('x2', '320')
+    .attr('y1','190').attr('y2', '190')
+    .attr('marker-start','url(#startarrow)').attr('marker-end','url(#endarrow)')
+    .transition(transitionPath)
+    .attr('x1', '353')
+
+  svg.append('text').attr('class','label').attr('fill','white')
+  .attr('transform', 'translate(360,180)').text('Petal').attr('opacity','0')
+  .transition(transitionPath)
+  .attr('opacity','1')
 }
 
-viz13();
+async function viz14(){
+  var svg = d3.select("#viz_1_3")
+  .select('svg')
 
+  const transitionPath = d3.transition().ease(d3.easeSin).duration(1000);
+  
+  svg.select('image').attr('opacity',1).transition(transitionPath).attr('opacity', 0)
+  svg.select('defs').selectAll('marker').selectAll('polygon').transition(transitionPath).attr('fill','black')
+  svg.selectAll('line.sepal').transition(transitionPath).attr('stroke','black')
+  svg.selectAll('line.petal').transition(transitionPath).attr('stroke','white')
+  svg.selectAll('line.petal').remove()
+  svg.selectAll('text.label').remove()
 
-d3.graphScroll()
-    .graph(d3.selectAll('#graph'))
-    .container(d3.select('#main'))
-    .sections(d3.selectAll('#s1_3_text'))
-    .on('active', function (i) {
-      viz13 ();
-    })
+  await delay(500);
+
+  svg.selectAll('line')
+
+  svg.append('ellipse').attr('fill','none').attr('rx','400').attr('ry','400').attr('cx','320').attr('cy','310')
+      .attr('stroke','black').attr('stroke-dasharray','4')
+      .transition().duration(1000).attr('rx','50').attr('ry','70')
+
+  svg.append('text').attr('class','label').attr('fill','black')
+    .text('versicolor').attr('transform','translate(285,225)').attr('opacity','0')
+    .transition().duration(1000).attr('opacity','1')
+
+  await delay(500);
+
+  // Setosa:
+  svg.append('line').attr('class','sepal')
+    .attr('stroke', 'black').attr('stroke-width', '2')
+    .attr('x1', '325').attr('x2', '325')
+    .attr('y1','350').attr('y2', '270')
+    .attr('marker-start','url(#startarrow)').attr('marker-end','url(#endarrow)')
+    .transition().duration(2000)
+    .attr('x1', '160').attr('x2','160')
+
+  svg.append('line').attr('class','sepal')
+    .attr('stroke', 'black').attr('stroke-width', '2')
+    .attr('x1', '355').attr('x2', '285')
+    .attr('y1','290').attr('y2', '290')
+    .attr('marker-start','url(#startarrow)').attr('marker-end','url(#endarrow)')
+    .transition().duration(2000)
+    .attr('x1', '200').attr('x2','120')
+    .attr('y2','310').attr('y1','310')
+
+  svg.append('ellipse').attr('fill','none').attr('rx','50').attr('ry','70').attr('cx','320').attr('cy','310')
+    .attr('stroke','black').attr('stroke-dasharray','4')
+    .transition().duration(2000)
+    .attr('rx','50').attr('ry','50').attr('cx','160')
+    
+  svg.append('text').attr('class','label').attr('fill','black')
+    .text('setosa').attr('transform','translate(135,225)').attr('opacity','0')
+    .transition().duration(1000).attr('opacity','1')
+
+  // Virginica:
+  svg.append('line').attr('class','sepal')
+    .attr('stroke', 'black').attr('stroke-width', '2')
+    .attr('x1', '325').attr('x2', '325')
+    .attr('y1','365').attr('y2', '255')
+    .attr('marker-start','url(#startarrow)').attr('marker-end','url(#endarrow)')
+    .transition().duration(2000)
+    .attr('x1', '500').attr('x2','500')
+
+  svg.append('line').attr('class','sepal')
+    .attr('stroke', 'black').attr('stroke-width', '2')
+    .attr('x1', '355').attr('x2', '285')
+    .attr('y1','290').attr('y2', '290')
+    .attr('marker-start','url(#startarrow)').attr('marker-end','url(#endarrow)')
+    .transition().duration(2000)
+    .attr('x1', '540').attr('x2','460')
+    .attr('y2','310').attr('y1','310')
+
+  svg.append('ellipse').attr('fill','none').attr('rx','50').attr('ry','70').attr('cx','320').attr('cy','310')
+    .attr('stroke','black').attr('stroke-dasharray','4')
+    .transition().duration(2000)
+    .attr('rx','50').attr('ry','70').attr('cx','500')
+    
+  svg.append('text').attr('class','label').attr('fill','black')
+    .text('virginica').attr('transform','translate(470,225)').attr('opacity','0')
+    .transition().duration(1000).attr('opacity','1')
+
+}
